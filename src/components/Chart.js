c8bc2ef8e1a20e202b11e27350ba8554226a40ac
titanium25 <idpm7N95tLPLskg>
2021-10-25 13:21:05 +0300

    chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -8,34 +8,57 @@ import HighchartsReact from 'highcharts-react-official'
 const Chart = () => {
 
     const [chart, setChart] = useState([]);
+    const [arr, setArr] = useState([]);
 
-    const options = {
-        title: {
-            text: 'My chart'
-        },
-        series: [{
-            name: 'Price',
-            data: [chart.High]
-        }]
-    }
+
 
     useEffect(async () => {
         const response = await BitCoinDAL.getAll();
+        response.data.data.map(v => arr.push([v.Date, v.High]))
         setChart(response.data.data)
-    }, [])
+    }, []);
+
+
+    const options = {
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'Bitcoin Price Chart'
+        },
+
+        series: [{
+            name: 'Bitcoin Price in USD',
+            data: arr,
+            type: 'area',
+            threshold: null,
+            tooltip: {
+                valueDecimals: 2
+            },
+
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            }
+        }]
+    }
 
     return (
         <div>
-
             <HighchartsReact
                 highcharts={Highcharts}
                 options={options}
             />
-
-
-            {chart.map((v,i) => {
-                return (<div key={i}>{v.High}</div>)
-            })}
         </div>
     );
 };
